b2103ad7d561016f184bb97ebe5c7d320a48f2ab
aishwaryayeole <aishwaryayeole@gmail.com>
2017-07-12 10:05:14 +0530

    Update JSmodular2.js

--- JSmodular2.js
+++ JSmodular2.js
@@ -9,19 +9,19 @@ this.accNo=accNo,
 	amt=2000;
 	var wAmt=1000;
 	return amt-wAmt;
-	}	,
+	}
 
 	function Deposit(amt)
 	{
 		amt=2000;
 		var dAmt=500;
 		return amt+dAmt;
-	},
+	}
 
 	function getAccBal()
 	{
 		return this.accBal;
-	},
+	}
 	
 	function getAccNo()
 	{
@@ -38,6 +38,5 @@ this.accNo=accNo,
 
 var objBankAcc = new BankAcc("Aish",100,1);
 document.write(objBankAcc.accName);
-
 document.write(BankAcc.MDeposit);
 
